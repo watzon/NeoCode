3c432192718e7f70bd8145de982ee2c72f14da19
Chris Watson <cawatson1993@gmail.com>
2026-03-13 10:36:04 -0600

    Initial Commit

---
+++ NeoCode/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NeoCode
+//
+//  Created by Chris W on 3/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NeoCodeTests/NeoCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeoCodeTests.swift
+//  NeoCodeTests
+//
+//  Created by Chris W on 3/13/26.
+//
+
+import Testing
+@testable import NeoCode
+
+struct NeoCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
